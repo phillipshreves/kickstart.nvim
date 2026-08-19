9491a85a9f61a5d036d16a28cca61d5743777390
Phillip Shreves <phil@highpointgo.com>
2026-08-19 09:44:11 -0500

    moved telescope search command from 's' to '/'

--- init.lua
+++ init.lua
@@ -433,45 +433,42 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>/h', builtin.help_tags, { desc = 'Search [H]elp' })
+      vim.keymap.set('n', '<leader>/k', builtin.keymaps, { desc = 'Search [K]eymaps' })
+      vim.keymap.set('n', '<leader>/f', builtin.find_files, { desc = 'Search [F]iles' })
+      vim.keymap.set('n', '<leader>/s', builtin.builtin, { desc = 'Search [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>/w', builtin.grep_string, { desc = 'Search current [W]ord' })
+      vim.keymap.set('n', '<leader>/g', builtin.live_grep, { desc = 'Search by [G]rep' })
+      vim.keymap.set('n', '<leader>/d', builtin.diagnostics, { desc = 'Search [D]iagnostics' })
+      vim.keymap.set('n', '<leader>/r', builtin.resume, { desc = 'Search [R]esume' })
+      vim.keymap.set('n', '<leader>/.', builtin.oldfiles, { desc = 'Search Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>/b', builtin.buffers, { desc = 'Search existing [b]uffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
+      vim.keymap.set('n', '<leader>//', function()
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {})
       end, { desc = '[/] Fuzzily search in current buffer' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
+      vim.keymap.set('n', '<leader>/o', function()
         builtin.live_grep {
           grep_open_files = true,
           prompt_title = 'Live Grep in Open Files',
         }
-      end, { desc = '[S]earch [/] in Open Files' })
+      end, { desc = 'Search in [O]pen Files' })
 
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
+      vim.keymap.set('n', '<leader>/n', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      end, { desc = 'Search [N]eovim files' })
 
       -- git
-      vim.keymap.set('n', '<leader>gst', builtin.git_status, { desc = '[G]it [St]atus' })
-      vim.keymap.set('n', '<leader>gco', builtin.git_commits, { desc = '[G]it [Co]mmits' })
-      vim.keymap.set('n', '<leader>gcb', builtin.git_bcommits, { desc = '[G]it [C]ommits [B]uffer' })
-      vim.keymap.set('n', '<leader>gbr', builtin.git_branches, { desc = '[G]it [Br]anches' })
-      vim.keymap.set('n', '<leader>gsh', builtin.git_stash, { desc = '[G]it [S]tas[h]' })
+      vim.keymap.set('n', '<leader>gits', builtin.git_status, { desc = '[Git] [St]atus' })
+      vim.keymap.set('n', '<leader>gitc', builtin.git_commits, { desc = '[Git] [C]ommits' })
+      vim.keymap.set('n', '<leader>gitb', builtin.git_bcommits, { desc = '[Git] Commits [B]uffer' })
+      vim.keymap.set('n', '<leader>gitr', builtin.git_branches, { desc = '[Git] B[r]anches' })
+      vim.keymap.set('n', '<leader>gith', builtin.git_stash, { desc = '[Git] Stas[h]' })
     end,
   },
 
